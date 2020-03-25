78537dd9b4ddcb37fc2f4680613fe19099d0af57
mathiascordova <macordova@miuandes.cl>
2020-03-25 14:41:24 -0300

    Incremento 1 completo

--- spotify/spotify/Program.cs
+++ spotify/spotify/Program.cs
@@ -10,11 +10,34 @@ namespace spotify
     {
         static void Main(string[] args)
         {
+            string seleccion;
             Espotifai spotify = new Espotifai();
-            Console.WriteLine(spotify.AgregarCancion(new Cancion("","","","")));
-            Console.WriteLine(spotify.AgregarCancion(new Cancion("","","","")));
-            Console.WriteLine(spotify.AgregarCancion(new Cancion("SAD!", "?", "XXXTENTACION", "Emo Rap")));
-            spotify.VerCanciones();
+            Console.WriteLine("Bienvenido a Espotifai!");
+            
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Indicar el numero al que se quiere acceder:");
+                Console.WriteLine("1. Ver canciones");
+                Console.WriteLine("2. Agregar una cancion");
+                Console.WriteLine("3. Salir de Espotifai");
+                seleccion = Console.ReadLine();
+                if (seleccion == "1")
+                {
+                    spotify.VerCanciones();
+                }
+                if (seleccion == "2")
+                {
+                    Console.WriteLine("Escriba nombre, album, artista y genero (en ese orden)");
+                    spotify.AgregarCancion(new Cancion(Console.ReadLine(), Console.ReadLine(), Console.ReadLine(), Console.ReadLine()));
+                }
+                else if (seleccion == "3")
+                {
+                    Console.WriteLine("Hasta Pronto!");
+                    break;
+                }
+            }
+            
         }
     }
 }
