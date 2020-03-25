9222e2046940b7dd1e99fba392bfe85b8d829e18
mathiascordova <macordova@miuandes.cl>
2020-03-25 12:26:59 -0300

    agrega clase cancion

---
+++ spotify/spotify/Cancion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spotify
+{
+    public class Cancion
+    {
+        private string nombre;
+        private string album;
+        private string artista;
+        private string genero;
+        public Cancion(string nombre, string album, string artista, string genero)
+        {
+            this.nombre = nombre;
+            this.album = album;
+            this.artista = artista;
+            this.genero = genero;
+        }
+
+        public string Informacion()
+        {
+            return ("Genero: " + genero + ", Artista: " + artista + ", Album: " + album + ", Nombre: " + nombre);
+        }
+    }
+}
---
+++ spotify/spotify/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spotify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cancion c1 = new Cancion("SAD!", "?", "XXXTENTACION", "Emo Rap");
+            Console.WriteLine(c1.Informacion());
+        }
+    }
+}
